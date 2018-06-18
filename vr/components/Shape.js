535725b90341a096d7feb79b13ab27489c483960
sleeplesseditor <josh@bryson.me.uk>
2018-06-18 16:45:10 +0100

    Game logic and score setup

--- vr/components/Shape.js
+++ vr/components/Shape.js
@@ -3,14 +3,17 @@ import { Box, Cylinder, Sphere } from 'react-vr';
 
 const shapes = [Box, Cylinder, Sphere];
 
+export { shapes };
+
 export default class Shape extends Component {
     render() {
         let Component = shapes[this.props.shapeNum];
+        let colors = ['#c33', '#3c3', '#33c', '#ccc'];
 
         return (
             <Component 
                 style={{ 
-                    color: '#fff',
+                    color: colors[this.props.colorNum],
                     transform: this.props.transform
                 }}
             />
